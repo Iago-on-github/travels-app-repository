63a87a1a180bb0eb777db43e10822813a2a599c9
Pichau <igorof06@outlook.com>
2025-09-28 00:31:43 -0300

    Refactor Student entity: add no-args constructor, remove @Table, add role field

--- src/main/java/com/travel_system/backend_app/model/Student.java
+++ src/main/java/com/travel_system/backend_app/model/Student.java
@@ -1,6 +1,7 @@
 package com.travel_system.backend_app.model;
 
 import com.travel_system.backend_app.model.enums.InstitutionType;
+import com.travel_system.backend_app.model.enums.Role;
 import com.travel_system.backend_app.model.enums.StatusStudent;
 import jakarta.persistence.*;
 
@@ -8,18 +9,25 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "students")
+//@Table(name = "students")
 @DiscriminatorValue("STUDENT")
 public class Student extends UserModel {
     private InstitutionType institutionType;
     private String course;
     private StatusStudent status;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+    public Student() {}
 
     public Student(UUID id, String email, String password, String name, String lastName, String telephone, String profilePicture, LocalDateTime createdAt, LocalDateTime updatedAt, InstitutionType institutionType, String course, StatusStudent status) {
         super(id, email, password, name, lastName, telephone, profilePicture, createdAt, updatedAt);
         this.institutionType = institutionType;
         this.course = course;
         this.status = status;
+        this.role = Role.ROLE_USER;
     }
 
     public InstitutionType getInstitutionType() {
@@ -46,5 +54,11 @@ public class Student extends UserModel {
         this.status = status;
     }
 
+    public Role getRole() {
+        return role;
+    }
 
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
