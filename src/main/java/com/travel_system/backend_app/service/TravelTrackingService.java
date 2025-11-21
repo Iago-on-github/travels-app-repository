313e0b233ad3efb7e8b1d7c9099663bfcc00eacd
Iago-on-Github <Iago737@outlook.com>
2025-11-21 20:36:36 -0300

    feat: desenvolvida a classe para orquestrar o sistema de tracking em tempo real - verifica desvio de rotas, recalcula ETA, salva localização e metadados da viagem

---
+++ src/main/java/com/travel_system/backend_app/service/TravelTrackingService.java
@@ -0,0 +1,94 @@
+package com.travel_system.backend_app.service;
+
+import com.travel_system.backend_app.exceptions.RecalculateEtaException;
+import com.travel_system.backend_app.exceptions.TravelException;
+import com.travel_system.backend_app.exceptions.TripNotFound;
+import com.travel_system.backend_app.model.Travel;
+import com.travel_system.backend_app.model.dtos.mapboxApi.PreviousStateDTO;
+import com.travel_system.backend_app.model.dtos.mapboxApi.RouteDetailsDTO;
+import com.travel_system.backend_app.model.dtos.mapboxApi.RouteDeviationDTO;
+import com.travel_system.backend_app.model.enums.TravelStatus;
+import com.travel_system.backend_app.repository.TravelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class TravelTrackingService {
+    private TravelRepository travelRepository;
+    private RedisTrackingService redisTrackingService;
+    private MapboxAPIService mapboxAPIService;
+
+    @Autowired
+    public TravelTrackingService(TravelRepository travelRepository, RedisTrackingService redisTrackingService, MapboxAPIService mapboxAPIService) {
+        this.travelRepository = travelRepository;
+        this.redisTrackingService = redisTrackingService;
+        this.mapboxAPIService = mapboxAPIService;
+    }
+
+    // Orquestra o sistema de tracking em tempo real, verificando desvios de rota,
+    // recalculando o ETA e salvando a localização e os metadados da viagem no Redis
+    public void processNewLocation(UUID travelId, Double currentLat, Double currentLng) {
+        Travel travel = travelRepository.findById(travelId)
+                .orElseThrow(() -> new TripNotFound("Trip not found"));
+
+        if (travel.getTravelStatus() != TravelStatus.TRAVELLING) {
+            throw new TravelException("A viagem não está em andamento");
+        }
+
+        RouteDeviationDTO routeDeviation = mapboxAPIService.isRouteDeviation(currentLat, currentLng, travel.getPolylineRoute());
+
+        RouteDetailsDTO newEtaRecalculateByApi;
+        PreviousStateDTO previousEta;
+
+        double newETARecalculateByInternally;
+
+        Double currentDuration;
+        Double currentDistance;
+        String currentPolyline;
+
+        try {
+            // se está fora da rota, chama o metodo para recalcular a distância entre os pontos
+            if (routeDeviation.isOffRoute()) {
+                newEtaRecalculateByApi = mapboxAPIService.recalculateETA(currentLng, currentLat, travel.getFinalLatitude(), travel.getFinalLongitude());
+
+                currentDuration = newEtaRecalculateByApi.duration();
+                currentDistance = newEtaRecalculateByApi.distance();
+                currentPolyline = newEtaRecalculateByApi.geometry();
+
+            } else {
+                previousEta = redisTrackingService.getPreviousEta(travel.getId().toString());
+
+                long currentTimeMillis = Instant.now().toEpochMilli();
+                long timeElapsedMillis = currentTimeMillis - previousEta.timeStamp();
+                double timeElapsedSeconds = (double) timeElapsedMillis / 1000.0;
+
+                newETARecalculateByInternally = previousEta.durationRemaining() - timeElapsedSeconds;
+
+                // nunca deixa ser valor negativo
+                newETARecalculateByInternally = Math.max(0.0, newETARecalculateByInternally);
+
+                currentDuration = newETARecalculateByInternally;
+                currentDistance = travel.getDistance();
+                currentPolyline = travel.getPolylineRoute();
+            }
+        } catch (RecalculateEtaException e) {
+            throw new RecalculateEtaException(e.getMessage());
+        }
+
+        redisTrackingService.storeLiveLocation(
+                travel.getId().toString(),
+                currentLat.toString(),
+                currentLng.toString(),
+                currentDuration.toString());
+
+        redisTrackingService.storeTravelMetadata(
+                travel.getId().toString(),
+                currentPolyline,
+                currentDistance.toString(),
+                travel.getTravelStatus().toString()
+        );
+    }
+}
