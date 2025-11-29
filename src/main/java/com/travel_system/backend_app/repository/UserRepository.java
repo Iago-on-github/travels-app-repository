e5dcdcef9f6e5f61b104e03f3440eaa0b95a5f53
Iago-on-Github <Iago737@outlook.com>
2025-11-28 21:03:05 -0300

    feat: adicionada novas regras de negócio enquanto a classe model e continuação das configurações do spring security

---
+++ src/main/java/com/travel_system/backend_app/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.travel_system.backend_app.repository;
+
+import com.travel_system.backend_app.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserDetails> findUserByEmail(String username);
+}
