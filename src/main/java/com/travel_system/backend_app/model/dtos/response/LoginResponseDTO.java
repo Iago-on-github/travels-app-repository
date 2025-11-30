dabb3a0a7b111c255f731ae1dda69a9acd075f22
Iago-on-Github <Iago737@outlook.com>
2025-11-30 16:48:07 -0300

    fix: Dto de resposta de autenticação refeito: adicionado mais campos para um retorno mais personalizado

--- src/main/java/com/travel_system/backend_app/model/dtos/response/LoginResponseDTO.java
+++ src/main/java/com/travel_system/backend_app/model/dtos/response/LoginResponseDTO.java
@@ -1,4 +1,13 @@
 package com.travel_system.backend_app.model.dtos.response;
 
-public record LoginResponseDTO(String token) {
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public record LoginResponseDTO(
+        String username,
+        Boolean authenticated,
+        Instant created,
+        Instant expiration,
+        String accessToken,
+        String refreshToken) {
 }
