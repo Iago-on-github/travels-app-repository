d2ef2fae2187ff5dfd432940919a730cae25aa17
Iago-on-Github <Iago737@outlook.com>
2025-12-03 20:37:06 -0300

    feat: adicionando novas regras de implementacao para criar um user (adm) com flag ADM

---
+++ src/main/java/com/travel_system/backend_app/model/dtos/request/AdministratorProfileDTO.java
@@ -0,0 +1,4 @@
+package com.travel_system.backend_app.model.dtos.request;
+
+public record AdministratorProfileDTO(String name, String email, String cpf, String password) {
+}
--- src/main/java/com/travel_system/backend_app/repository/UserRepository.java
+++ src/main/java/com/travel_system/backend_app/repository/UserRepository.java
@@ -2,13 +2,13 @@ package com.travel_system.backend_app.repository;
 
 import com.travel_system.backend_app.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
     UserModel findUserByEmail(String email);
+
+    UserModel findUserByCpf(String cpf);
 }
