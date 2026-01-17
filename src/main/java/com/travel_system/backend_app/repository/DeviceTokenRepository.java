8ecb3f06a52965d86db4e1ca6ac83db26b68d868
Iago-on-Github <Iago737@outlook.com>
2026-01-16 22:27:17 -0300

    feat: - adição da funcionalidade de pegar todos os tokens do usuário - começo do desenvolvimento do método de enviar as notificações ao firebase

--- src/main/java/com/travel_system/backend_app/repository/DeviceTokenRepository.java
+++ src/main/java/com/travel_system/backend_app/repository/DeviceTokenRepository.java
@@ -4,11 +4,12 @@ import com.travel_system.backend_app.model.DeviceToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface DeviceTokenRepository extends JpaRepository<DeviceToken, UUID> {
     Optional<DeviceToken> findDeviceTokenByToken(String token);
+
 }
--- src/main/java/com/travel_system/backend_app/utils/FirebaseNotificationSender.java
+++ src/main/java/com/travel_system/backend_app/utils/FirebaseNotificationSender.java
@@ -1,20 +1,24 @@
 package com.travel_system.backend_app.utils;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.travel_system.backend_app.exceptions.DeviceTokenNotFoundException;
 import com.travel_system.backend_app.exceptions.DomainValidationException;
 import com.travel_system.backend_app.exceptions.InvalidDeviceTokenException;
 import com.travel_system.backend_app.model.DeviceToken;
 import com.travel_system.backend_app.model.Student;
 import com.travel_system.backend_app.model.Travel;
+import com.travel_system.backend_app.model.dtos.VehicleMovementNotificationDTO;
+import com.travel_system.backend_app.model.enums.MovementState;
 import com.travel_system.backend_app.model.enums.Platform;
+import com.travel_system.backend_app.model.enums.Priority;
 import com.travel_system.backend_app.repository.DeviceTokenRepository;
 import com.travel_system.backend_app.repository.StudentRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.time.Instant;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,16 +26,20 @@ public class FirebaseNotificationSender {
 
     private final DeviceTokenRepository deviceTokenRepository;
     private final StudentRepository studentRepository;
+    private final FirebaseMessaging firebaseMessaging;
 
-    public FirebaseNotificationSender(DeviceTokenRepository deviceTokenRepository, StudentRepository studentRepository) {
+    public FirebaseNotificationSender(DeviceTokenRepository deviceTokenRepository, StudentRepository studentRepository, FirebaseMessaging firebaseMessaging) {
         this.deviceTokenRepository = deviceTokenRepository;
         this.studentRepository = studentRepository;
+        this.firebaseMessaging = firebaseMessaging;
     }
 
     // proximos passos:
     // buscar todos os tokens ativos do student
     // enviar notificação para cada um
     // trtar falhas do firebase (token inválido: active = false)
+
+    // registra/atualiza os tokens do usuário
     public void manageUserToken(Student student, String token, Platform platform) {
         if (student == null || token == null || token.isBlank() || platform == null) throw new DomainValidationException("Token is null");
 
@@ -53,4 +61,30 @@ public class FirebaseNotificationSender {
         deviceToken.setPlatform(platform);
         deviceTokenRepository.save(deviceToken);
     }
+
+    // pega todos os tokens ativos do usuário
+    public Set<DeviceToken> studentActiveTokens(UUID studentId) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new EntityNotFoundException("estudante não encontrado" + studentId));
+
+        return student.getDeviceTokens()
+                .stream().filter(DeviceToken::isActive).collect(Collectors.toSet());
+    }
+
+    // enviar notificação ao firebase
+    public VehicleMovementNotificationDTO pushNotificationToFirebase(UUID studentId, UUID travelId, MovementState movementState, Priority priority, String message) {
+        Set<DeviceToken> studentActiveTokens = studentActiveTokens(studentId);
+
+        if (studentActiveTokens.isEmpty()) return null;
+
+        VehicleMovementNotificationDTO movementNotificationDTO = new VehicleMovementNotificationDTO(travelId, movementState, Instant.now(), message, priority);
+
+        Message payload =;
+
+        studentActiveTokens.forEach(deviceToken -> {
+            firebaseMessaging.send(payload);
+        });
+    }
+
+    // tratar falhas
 }
