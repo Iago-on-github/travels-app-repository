94f7a9acc5bf4974257fc15f318c6cc2cb789097
Iago device note <Iago737@outlook.com>
2026-02-25 22:46:31 -0300

    fix - remoção da routing key de gps

--- src/main/java/com/travel_system/backend_app/config/RabbitMQConfig.java
+++ src/main/java/com/travel_system/backend_app/config/RabbitMQConfig.java
@@ -1,14 +1,11 @@
 package com.travel_system.backend_app.config;
 
-import com.google.api.client.util.Value;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.listener.Topic;
 
 @Configuration
 public class RabbitMQConfig {
@@ -19,9 +16,6 @@ public class RabbitMQConfig {
 
     public static final String NOTIFICATION_ROUTE_KEY = "notification_distance";
     // definir: gps.<id_cidade>.<id_linha>.<id_veiculo>
-    public static final String GPS_ROUTE_KEY = "";
-
-    TopicExchange amqTopic = new TopicExchange("amq.topic");
 
     @Bean
     public Queue queue() {
@@ -41,6 +35,7 @@ public class RabbitMQConfig {
     // faz o mqtt olhar para a exchange custom e não para a padrão amqTopic
     @Bean
     public Binding bindGpsExchangeToTopicExchange() {
+        TopicExchange amqTopic = new TopicExchange("amq.topic");
         return BindingBuilder.bind(amqTopic).to(exchangeGps()).with("#");
     }
 
