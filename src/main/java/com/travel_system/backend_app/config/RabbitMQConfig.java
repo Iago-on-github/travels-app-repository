3d06d9a4cc63bb720b243df2ea348f001ffce967
Iago device note <Iago737@outlook.com>
2026-02-28 23:09:41 -0300

    feat/fix: - inserção de novas constantes para: queues, exchanges e routing keys - criação de novas queues, exchanges e bindings para estrategias de parking lot - refatoração na queue de notiicação para adequa-la à estrategia de parking lot

--- src/main/java/com/travel_system/backend_app/config/RabbitMQConfig.java
+++ src/main/java/com/travel_system/backend_app/config/RabbitMQConfig.java
@@ -9,16 +9,36 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String QUEUE_NOTIFICATION_NAME = "queue_notification";
+    public static final String QUEUE_NOTIFICATION_NAME = "queue.notification";
+    public static final String QUEUE_ERR_DLQ = "dlq.queue";
 
-    public static final String EXCHANGE_NOTIFICATION_NAME = "push_distance_notification";
-    public static final String EXCHANGE_GPS_NAME = "tg_gps_exchange";
+    public static final String EXCHANGE_NOTIFICATION_NAME = "push.distance.notification";
+    public static final String EXCHANGE_GPS_NAME = "tg.gps.exchange";
+    public static final String EXCHANGE_ERR_DLX = "notification.dlx";
 
-    public static final String NOTIFICATION_ROUTE_KEY = "notification_distance";
+    public static final String NOTIFICATION_ROUTE_KEY = "notification.distance";
+    public static final String DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
+    public static final String ERR_ROUTING_KEY = "notification.error";
+
+    public static final String QUEUE_PARKING_LOT = QUEUE_NOTIFICATION_NAME + ".parking-lot";
+    public static final String EXCHANGE_PARKING_LOT = QUEUE_NOTIFICATION_NAME + ".exchange.parking-lot";
 
     @Bean
-    public Queue queue() {
-        return new Queue(QUEUE_NOTIFICATION_NAME, true);
+    public Queue queueNotification() {
+        return QueueBuilder.durable()
+                .withArgument("x-dead-letter-exchange", EXCHANGE_ERR_DLX)
+                .withArgument(DEAD_LETTER_ROUTING_KEY, ERR_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public Queue queueErr() {
+        return new Queue(QUEUE_ERR_DLQ, true);
+    }
+
+    @Bean
+    public Queue queueParkingLot() {
+        return new Queue(QUEUE_PARKING_LOT, true);
     }
 
     @Bean
@@ -31,6 +51,16 @@ public class RabbitMQConfig {
         return new TopicExchange(EXCHANGE_GPS_NAME);
     }
 
+    @Bean
+    public TopicExchange exchangeError() {
+        return new TopicExchange(EXCHANGE_ERR_DLX);
+    }
+
+    @Bean
+    public TopicExchange parkingLotExchange() {
+        return new TopicExchange(EXCHANGE_PARKING_LOT);
+    }
+
     // faz o mqtt olhar para a exchange custom e não para a padrão amqTopic
     @Bean
     public Binding bindGpsExchangeToTopicExchange() {
@@ -38,10 +68,19 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(amqTopic).to(exchangeGps()).with("#");
     }
 
+    @Bean
+    public Binding bindingNotification(Queue queueNotification, TopicExchange exchangeNotification) {
+        return BindingBuilder.bind(queueNotification).to(exchangeNotification).with(NOTIFICATION_ROUTE_KEY);
+    }
 
     @Bean
-    public Binding bindingNotification(Queue queue, TopicExchange exchangeNotification) {
-        return BindingBuilder.bind(queue).to(exchangeNotification).with(NOTIFICATION_ROUTE_KEY);
+    public Binding bindingErr(Queue queueErr, TopicExchange exchangeErr) {
+        return BindingBuilder.bind(queueErr).to(exchangeErr).with(ERR_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding bindingParkingLot(Queue queueParkingLot, TopicExchange exchangeParkingLot) {
+        return BindingBuilder.bind(queueParkingLot).to(exchangeParkingLot).with(DEAD_LETTER_ROUTING_KEY);
     }
 
     // serialização
