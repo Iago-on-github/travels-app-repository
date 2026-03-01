e10d2fbe8b7ab87fd026bd52c2890cb130b4270f
Iago device note <Iago737@outlook.com>
2026-02-28 23:07:00 -0300

    feat: - definição de novo contrato para reprocessar failed messages com estrategia de parking-lot

--- src/main/java/com/travel_system/backend_app/interfaces/NotificationMessagingContract.java
+++ src/main/java/com/travel_system/backend_app/interfaces/NotificationMessagingContract.java
@@ -1,8 +1,11 @@
 package com.travel_system.backend_app.interfaces;
 
 import com.travel_system.backend_app.model.dtos.mesageria.SendPackageDataToRabbitMQ;
+import org.springframework.amqp.core.Message;
 
 public interface NotificationMessagingContract {
 
     void sendMessage(SendPackageDataToRabbitMQ event);
+
+    void processFailedMessagesRetryWithParkingLotStrategy(Message failedMessage);
 }
