cde8a24b0756a43fc6920aba589f740c10a3b234
Iago device note <Iago737@outlook.com>
2026-02-28 23:08:17 -0300

    feat: - inserçaão de novo mapeamento para failedMessages

--- src/main/java/com/travel_system/backend_app/utils/RabbitMQConsumer.java
+++ src/main/java/com/travel_system/backend_app/utils/RabbitMQConsumer.java
@@ -2,14 +2,25 @@ package com.travel_system.backend_app.utils;
 
 import com.travel_system.backend_app.config.RabbitMQConfig;
 import com.travel_system.backend_app.model.dtos.mesageria.SendPackageDataToRabbitMQ;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQConsumer {
 
+    private final Logger logger = LoggerFactory.getLogger(RabbitMQConsumer.class);
+
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NOTIFICATION_NAME)
     public void receiveMessages(SendPackageDataToRabbitMQ event) {
-        System.out.println("Received message: " + event);
+        logger.info("Received message: {}", event);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_ERR_DLQ)
+    public void processFailedMessages(Message message) {
+        logger.info("Received failed message: {}", message.toString());
+        // salvar no DB ou enviar a notificação
     }
 }
