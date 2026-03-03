37dafced16ccee9cfc640d896aed7b4c266099d3
Iago device note <Iago737@outlook.com>
2026-03-02 21:22:11 -0300

    fix: - renomeaçao de método notificition

--- src/main/java/com/travel_system/backend_app/utils/NotificationConsumer.java
+++ src/main/java/com/travel_system/backend_app/utils/NotificationConsumer.java
@@ -4,23 +4,16 @@ import com.travel_system.backend_app.config.RabbitMQConfig;
 import com.travel_system.backend_app.model.dtos.mesageria.SendPackageDataToRabbitMQ;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RabbitMQConsumer {
+public class NotificationConsumer {
 
-    private final Logger logger = LoggerFactory.getLogger(RabbitMQConsumer.class);
+    private final Logger logger = LoggerFactory.getLogger(NotificationConsumer.class);
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NOTIFICATION_NAME)
     public void receiveMessages(SendPackageDataToRabbitMQ event) {
         logger.info("Received message: {}", event);
-    }
-
-    @RabbitListener(queues = RabbitMQConfig.QUEUE_ERR_DLQ)
-    public void processFailedMessages(Message message) {
-        logger.info("Received failed message: {}", message.toString());
-        // salvar no DB ou enviar a notificação
     }
 }
