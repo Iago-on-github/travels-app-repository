ca548e64e04e5fa09a1caa9fde0f3854b6c08635
Iago device note <Iago737@outlook.com>
2026-03-09 23:03:58 -0300

    feat/fix - implementação da funcionaliade de registro de histórico de ping no método que recebe os dados do front

--- src/main/java/com/travel_system/backend_app/service/GpsDataIngestorService.java
+++ src/main/java/com/travel_system/backend_app/service/GpsDataIngestorService.java
@@ -8,26 +8,33 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class GpsDataIngestorService {
 
     private final RabbitTemplate rabbitTemplate;
+    private final RedisTrackingService redisTrackingService;
+    private final TravelHistoryPingsService travelHistoryPingsService;
 
-    public GpsDataIngestorService(RabbitTemplate rabbitTemplate) {
+    public GpsDataIngestorService(RabbitTemplate rabbitTemplate, RedisTrackingService redisTrackingService, TravelHistoryPingsService travelHistoryPingsService) {
         this.rabbitTemplate = rabbitTemplate;
+        this.redisTrackingService = redisTrackingService;
+        this.travelHistoryPingsService = travelHistoryPingsService;
     }
 
     // envia ao rabbitmq as informações de gps da viagem + a routing_key contendo ids específicos
     public void sendVehicleGps(String city, String travelId, VehicleLocationRequestDTO vehicleLocation) {
         final String ROUTING_KEY = "v1.gps." + city + "." + travelId;
 
+        Instant now = Instant.now();
+
         GpsPayload gpsPayload = new GpsPayload(
                 vehicleLocation.latitude(),
                 vehicleLocation.longitude(),
                 vehicleLocation.speed(),
                 vehicleLocation.heading(),
-                Instant.now(),
+                now,
                 vehicleLocation.travelId()
         );
 
@@ -36,6 +43,16 @@ public class GpsDataIngestorService {
             location.getMessageProperties().setDeliveryMode(MessageDeliveryMode.NON_PERSISTENT);
             return location;
         });
+
+        // datahistory dos pings durante a viagem
+        boolean locationUpdateAllowed = redisTrackingService.isLocationUpdateAllowed(UUID.fromString(travelId));
+
+        if (!locationUpdateAllowed) return;
+
+        travelHistoryPingsService.saveTravelLocationHistoryData(city, travelId, now, vehicleLocation);
+
+        // salva os novos pings que chegarão
+        redisTrackingService.saveHistoryPingLocation(UUID.fromString(travelId), now);
     }
 
 
